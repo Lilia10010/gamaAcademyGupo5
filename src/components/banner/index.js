228396a1d88b1b1a98a5c6ab984d8cfe5687e04a
Renato Napoli <renato.napoli@nagro.app>
2022-05-29 13:25:20 -0300

    Ajustes layout

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,18 +3,14 @@ import carro from "../../img/ilustracoes/carro-banner.png";
 import "../../css/banner.css";
 
 const banner = `
-<container class="containerBanner">
 <img id="imagemFundo" src="${imagemFundo}" />
+<container class="containerBanner">
 <div class="conteudoBanner">
 <h1 id="tituloBanner">Compre e venda</br> com facilidade.</h1>
 <p id="subtituloBanner">O Finder é um mercado digital líder para a<br/>
 indústria automotiva que conecta compradores<br/> de carros a vendedores.</p>
 </div>
 <img id="carroBanner" src="${carro}">
-
-
-
-
 </container>
 `;
 
--- src/components/menuSelect/index.js
+++ src/components/menuSelect/index.js
@@ -7,35 +7,50 @@ import local from "../../img/icons/local.png";
 import "../../css/menuSelect.css";
 
 const menuSelect = `
+
 <div class="menuBotoes">
+
+<div>
 <button id="botaoNovo">Novo</button>
 <button id="botaoUsado">Usado</button>
 </div>
 
+
 <div class="menuSelecao">
 <div id="inputPesquisar">
 <img src="${lupaPesquisar}"/>
-<input type="text" id="pesquisar" alt="buscar" placeholder="Perquisar por ...." />
+<input type="text" id="pesquisar" alt="buscar" placeholder="Pesquisar por ...." />
+</div>
 <img src="${barraVerticalMenu}"/>
+<div>
 <img id="imagemEstrela"src="${estrelaMarca}"/>
 <select id="selectMarca">
 <option>Marca</option>
 </select>
+</div>
 <img src="${barraVerticalMenu}"/>
+<div>
 <img id="modeloCarro"src="${modeloCarro}"/>
 <select id="selectModelo">
 <option>Modelo</option>
 </select>
+</div>
 <img src="${barraVerticalMenu}"/>
+<div>
 <img id="tipoCarro"src="${tipoCarro}"/>
 <select id="selectTipo">
 <option>Tipo</option>
 </select>
+</div>
 <img src="${barraVerticalMenu}"/>
+<div id="containerBotaoPesquisar">
 <img id="local"src="${local}"/>
 <select id="selectLocal">
 <option>Local</option>
 </select>
+<div>
+</div>
+</div>
 <button id="botaoPesquisar">Pesquisar</button>
 </div>
 </div>
