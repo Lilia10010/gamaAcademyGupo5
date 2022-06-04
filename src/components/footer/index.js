1949b58a637635d4d3acaabe867ea175e244523b
lucasdev155 <63871665+lucasdev155@users.noreply.github.com>
2022-06-04 20:57:57 -0300

    footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,92 @@
-let footer = `<div>
-    <h1>rodapé</h1>
-</div>`;
+import "../../css/footer.css"
+import Logo from "../../img/icons/lupa.png";
+import Local from "../../img/icons/local.png";
+import Pais from "../../img/icons/pais.png";
+import Seta from "../../img/icons/seta.png";
+import Telefone from "../../img/icons/telefone.png";
+import Email from "../../img/icons/email.png";
+import Face from "../../img/icons/face.png";
+import Twitter from "../../img/icons/twitter.png";
+import Telegram from "../../img/icons/telegram.png";
+import Messeger from "../../img/icons/messeger.png";
+import Vector from "../../img/icons/vector.png";
+import Finder from "../../img/icons/finder.png";
+
+let footer = `<footer>
+<div class="finder">
+    <div id="circulo">
+      <img id="img-logo" src="${Logo}"/>
+      <img id="finder" src="${Finder}"/>
+    </div>
+  <div class="localizacao">
+    <h3><img src="${Pais}"/> pt-BR <img src="${Seta}"/></h3>
+    <h3><img src="${Local}"/> São Paulo <img src="${Seta}"/></h3>
+  </div>
+</div>
+<div class="container-footer">
+    <ul class="content-footer">
+      <div>
+        <h1 class="newsletter">Cadastre-se na newsletter</h1>
+        <p id="aviso">Não perca nenhuma oferta relevante!</p>
+        <form>
+          <input class="input" type="text" placeholder="Informe seu email"/>
+          <button class="button"><img src="${Vector}"/></button>
+        </form>
+      </div>
+      <li>
+          <h2 class="titulo-lista">Comprar ou Vender</h2>
+          <ul class="box">
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+          </ul>
+      </li>
+      <li class="features">
+          <h2 class="titulo-lista">Sobre</h2>
+          <ul class="box">
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+          </ul>
+      </li>
+      <li>
+          <h2 class="titulo-lista">Perfil</h2>
+          <ul class="box">
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+              <li><a href="#">Loren ipsun</a></li>
+          </ul>
+      </li>
+      <li class="contato">
+        <ul>
+          <div class="phone">
+          <a class="phone"><img src="${Telefone}"/>(11) 1111-1111</a>
+          </div>
+          <div class="email">
+          <a href="#"><img src="${Email}"/>email@finder.com.br</a>
+          </div>
+          <div class="icones">
+            <a href="#"><img src="${Face}"/></a>
+            <a href="#"><img src="${Twitter}"/></a>
+            <a href="#"><img src="${Telegram}"/></a>
+            <a id="messeger" href="#"><img src="${Messeger}"/></a>
+          </div>
+        </ul>
+      </li>
+    </ul>
+</div>
+<div class="termos">
+  <p>Copyright © Finder | Gama Academy. Todos os direitos reservados - Este é um projeto acadêmico.</p>
+  <div>
+    <p>Termos de uso</p>
+    <p>Política de privacidade</p>
+  </div>
+</div>
+</footer>`;
 
 export default footer;
