93bd6429772266c8b0a2051f3be520b7078e8bb8
Renato Napoli <renato.napoli@nagro.app>
2022-06-09 15:05:24 -0300

    Feat: layout carrossel

--- src/components/lastPublish/index.js
+++ src/components/lastPublish/index.js
@@ -1,6 +1,10 @@
 import "../../css/lastPublish.css";
+import ChevronLeftIcon from "@img/icons/svg/chevronLeft.svg";
+import ChevronRightIcon from "@img/icons/svg/chevronRight.svg";
 import ArrowIcon from "@img/icons/svg/arrow.svg";
 import Mazda from "@img/cars/mazda.png";
+import Bmw_640 from "@img/cars/bmw_640_Xi.png";
+import Mercedes from "@img/cars/mercedes.png";
 import Local from "@img/icons/local.png";
 import Velocimetro from "@img/icons/velocimetro.png";
 import Combustivel from "@img/icons/combustivel.png";
@@ -17,9 +21,13 @@ const lastPublish = `
 </div>
 </div>
 <div id="containerCardPublish">
-    <div id="containerCarrosselPublish"> 
+    <div id="containerCarrosselPublish">
+    <img src="${ChevronLeftIcon}" /> 
       <div id="cardPublish">
-        <img src="${Mazda}" />
+        <img id="imgMazda" src="${Mazda}" />  
+        <div id="bordaNovo">
+          <p id="tituloNovo">Novo</p>
+        </div>
         <div id="anoModelo">
           <p id="data">2019</p>
         <div id="checkboxCard">
@@ -50,7 +58,10 @@ const lastPublish = `
       </div>      
     </div>
     <div id="cardPublish">
-        <img src="${Mazda}" />
+        <img src="${Bmw_640}" />
+        <div id="bordaUsado">
+          <p id="tituloUsado">Usado</p>
+        </div>
         <div id="anoModelo">
           <p id="data">2019</p>
         <div id="checkboxCard">
@@ -58,30 +69,33 @@ const lastPublish = `
         <label id="tituloCheckbox" for="comprar">Comprar</label>
       </div>
     </div>
-      <h5 id="tituloCardCarro">Mazda MX-5 Miata Convertible</h5>
-      <p id="precoCard">$38,500</p>
+      <h5 id="tituloCardCarro">BMW 640 XI Gran Turismo </h5>
+      <p id="precoCard">$44,920</p>
       <div id="local">
         <img src="${Local}" />
-        <p>Los Angeles</p>
+        <p>New Jersey</p>
       </div>
       <hr id="linhaVertical" />   
       <div id="botoesCardsCarrossel">
         <button id="containerKilometragem">
           <img  src="${Velocimetro}" />
-          <p>18k mi</p>
+          <p>10k mi</p>
         </button>
         <button id="containerCambio">
           <img  src="${Cambio}" />
-          <p>Manaul</p>
+          <p>Automatic</p>
         </button>
         <button id="containerCombustivel">
           <img  src="${Combustivel}" />
-          <p>Petrol</p>
+          <p>Gasoline</p>
         </button>
       </div>      
     </div>
     <div id="cardPublish">
-        <img src="${Mazda}" />
+        <img src="${Mercedes}" />
+        <div id="bordaUsado">
+          <p id="tituloUsado">Usado</p>
+        </div>
         <div id="anoModelo">
           <p id="data">2019</p>
         <div id="checkboxCard">
@@ -111,6 +125,12 @@ const lastPublish = `
         </button>
       </div>      
     </div>
+    <img src="${ChevronRightIcon}" />
+  </div>
+  <div id="indicadores">
+  <div id="indicadorImg"></div>
+  <div id="indicadorImg"></div>
+  <div id="indicadorImg"></div>
   </div>
 </div>
 `;
