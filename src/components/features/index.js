16745f443500019ce9e8a6a58c12184f6857b21a
neilsonsp <neilsonsilvap@gmail.com>
2022-06-13 09:56:51 -0400

    ajustes no CSS

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -16,7 +16,8 @@ let Features = `
   <div class="header">
     <h2 class="title">O que temos de diferente</h2>
     <a href="#" title="Saiba como!" class="view-more">
-      <h3>Como vender carros no Finder <img src="${ArrowIcon}"/></h3>
+      <h3>Como vender carros no Finder</h3>
+      <img src="${ArrowIcon}"/>
     </a>
   </div>
   <div class="containerFeatures">
